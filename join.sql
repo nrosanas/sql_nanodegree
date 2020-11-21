f9a5c5172e49542192fc3d98bbc6a0112bfb397c
Noè Rosanas <noe.merles@gmail.com>
2020-11-21 16:48:10 +0100

    començant el tema 2

---
+++ join.sql
@@ -0,0 +1,12 @@
+/* 
+#########################
+#                       #
+#         JOIN          #
+#                       #
+#########################
+*/
+
+SELECT orders.*
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
